e8f2a7b0aa74f24ad9f12909af01e3839abecd69
yucan.zhang <zhang111307@163.com>
2020-09-02 11:12:52 +0800

    增加正则工具

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,23 +1,5 @@
 module devAide {
 
-    exports org.jflame.devAide.component;
-    // exports org.jflame.devAide.util;
-    exports org.jflame.devAide.component.convertor;
-    exports org.jflame.devAide.model;
-    exports org.jflame.devAide.plugin;
-
-    exports org.jflame.devAide to javafx.graphics;
-    exports org.jflame.devAide.controller to javafx.graphics, javafx.fxml;
-
-    opens org.jflame.devAide.controller to javafx.fxml;
-    opens org.jflame.devAide.component to javafx.fxml;
-
-    uses org.jflame.devAide.plugin.ToolPlugin;
-
-    provides org.jflame.devAide.plugin.ToolPlugin
-            with org.jflame.devAide.plugin.BarcodeTool, org.jflame.devAide.plugin.CronTool,
-            org.jflame.devAide.plugin.FormatTool, org.jflame.devAide.plugin.RegexTool;
-
     requires com.google.zxing;
     requires com.google.zxing.javase;
     requires org.controlsfx.controls;
@@ -33,4 +15,21 @@ module devAide {
     requires fastjson;
     requires jflame.commons;
     requires slf4j.api;
+
+    exports org.jflame.devAide.component;
+    exports org.jflame.devAide.model;
+    exports org.jflame.devAide.plugin;
+
+    exports org.jflame.devAide to javafx.graphics;
+    exports org.jflame.devAide.controller to javafx.graphics, javafx.fxml;
+
+    opens org.jflame.devAide.controller to javafx.fxml;
+    opens org.jflame.devAide.component to javafx.fxml;
+
+    uses org.jflame.devAide.plugin.ToolPlugin;
+
+    provides org.jflame.devAide.plugin.ToolPlugin
+            with org.jflame.devAide.plugin.BarcodeTool, org.jflame.devAide.plugin.CronTool,
+            org.jflame.devAide.plugin.FormatTool, org.jflame.devAide.plugin.RegexTool;
+
 }
--- src/main/java/org/jflame/devAide/controller/RegexController.java
+++ src/main/java/org/jflame/devAide/controller/RegexController.java
@@ -1,13 +1,24 @@
 package org.jflame.devAide.controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.StringUtils;
+import org.controlsfx.validation.ValidationResult;
+import org.controlsfx.validation.ValidationSupport;
+import org.controlsfx.validation.Validator;
+import org.jflame.devAide.component.MyGraphicValidationDecoration;
 import org.jflame.devAide.util.UIComponents;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Control;
 import javafx.scene.control.TextArea;
 
 public class RegexController {
@@ -32,9 +43,14 @@ public class RegexController {
     private CheckBox chxMultiMode;
     @FXML
     private CheckBox chxReplace;
+    @FXML
+    private CheckBox chxDotall;
 
     @FXML
     private TextArea replText;
+
+    private PattenOption pattenOption = new PattenOption();
+    private ValidationSupport validationSupport = new ValidationSupport();
 
     public RegexController() {
 
@@ -42,21 +58,45 @@ public class RegexController {
 
     @FXML
     private void initialize() {
-        // 替换模式
+        replText.setManaged(false);
+        // 绑定属性
         chxReplace.selectedProperty()
                 .addListener(new ChangeListener<Boolean>() {
 
                     @Override
                     public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue,
                             Boolean newValue) {
+                        System.out.println(newValue);
+                        System.out.println(chxReplace.isSelected());
                         if (newValue) {
-                            UIComponents.hide(replText);
+                            UIComponents.show(replText);
                             replText.setText(null);
                         } else {
-                            UIComponents.show(replText);
+                            UIComponents.hide(replText);
                         }
                     }
                 });
+        chxCase.selectedProperty()
+                .bindBidirectional(pattenOption.caseInsensitive());
+        chxMultiMode.selectedProperty()
+                .bindBidirectional(pattenOption.multiline());
+        chxDotall.selectedProperty()
+                .bindBidirectional(pattenOption.dotall());
+
+        validationSupport.setValidationDecorator(new MyGraphicValidationDecoration());
+        validationSupport.registerValidator(srcText, Validator.createEmptyValidator("请输入源文本"));
+        validationSupport.registerValidator(regexText, Validator.createEmptyValidator("请输入正则表达式"));
+        validationSupport.registerValidator(replText, new Validator<String>() {
+
+            @Override
+            public ValidationResult apply(Control t, String u) {
+                if (chxReplace.isSelected()) {
+                    return ValidationResult.fromErrorIf(t, "请输入替换文本", StringUtils.isBlank(replText.getText()));
+                }
+                return null;
+            }
+        });
+
         /* ToggleGroup optToggleGrp = new ToggleGroup();
         ToggleButton searchToggleBtn = new ToggleButton("查找");
         searchToggleBtn.setSelected(true);
@@ -98,13 +138,99 @@ public class RegexController {
                 .buildAll();
         leftgrid.add(resultBorder, 0, 3);*/
 
-        replText.setManaged(false);
-
     }
 
     @FXML
     protected void handleRegex(ActionEvent event) {
+        System.out.println("check:" + validationSupport.isInvalid());
+        if (!validationSupport.isInvalid()) {
+            Pattern regex = Pattern.compile(regexText.getText(), pattenOption.or());
+            Matcher matcher = regex.matcher(srcText.getText());
+            if (chxReplace.isSelected()) {
+                String replResult = matcher.replaceAll(replText.getText());
+                resultText.setText("替换结果: " + replResult);
+            } else {
+                StringBuilder matchResult = new StringBuilder();
+                int i = 1;
+                while (matcher.find()) {
+                    matchResult.append("匹配结果" + i + ": ");
+                    matchResult.append(matcher.group());
+                    matchResult.append("\n");
+                    i++;
+                }
+                if (matchResult.length() == 0) {
+                    resultText.setText("无匹配结果");
+                } else {
+                    resultText.setText(matchResult.toString());
+                }
+            }
+        }
+    }
+
+    class PattenOption {
+
+        private BooleanProperty caseInsensitive = new SimpleBooleanProperty(false);
+        private BooleanProperty multiline = new SimpleBooleanProperty(false);
+        private BooleanProperty dotall = new SimpleBooleanProperty(false);
+
+        public boolean getCaseInsensitive() {
+            return caseInsensitive.get();
+        }
+
+        public BooleanProperty caseInsensitive() {
+            return caseInsensitive;
+        }
+
+        public void setCaseInsensitive(boolean _caseInsensitive) {
+            caseInsensitive.set(_caseInsensitive);
+        }
+
+        public boolean getMultiline() {
+            return multiline.get();
+        }
+
+        public BooleanProperty multiline() {
+            return multiline;
+        }
+
+        public void setMultiline(boolean _multiline) {
+            this.multiline.set(_multiline);
+        }
+
+        public boolean getDotall() {
+            return dotall.get();
+        }
+
+        public BooleanProperty dotall() {
+            return dotall;
+        }
+
+        public void setDotall(boolean _dotall) {
+            this.dotall.set(_dotall);
+        }
+
+        public int or() {
+            int i = 0;
+            if (getCaseInsensitive()) {
+                i = i | Pattern.CASE_INSENSITIVE;
+            }
+            if (getDotall()) {
+                i = i | Pattern.DOTALL;
+            }
+            if (getMultiline()) {
+                i = i | Pattern.MULTILINE;
+            }
+            return i;
+        }
 
     }
 
+    /*    public static void main(String[] args) {
+        PattenOption ops = new PattenOption();
+        System.out.println(ops.or());
+        ops.setCaseInsensitive(true);
+        System.out.println(ops.or());
+        ops.setMultiline(true);
+        System.out.println(ops.or());
+    }*/
 }
